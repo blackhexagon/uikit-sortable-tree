03ab411149ec0dc5f9590700e8d3efbfe730ae42
Matous Trca <matous.trca@gmail.com>
2020-11-18 11:48:41 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,134 @@
-import logo from './logo.svg';
-import './App.css';
+import SortableTree, {
+  changeNodeAtPath,
+  addNodeUnderParent,
+  removeNodeAtPath,
+  getTreeFromFlatData,
+  getFlatDataFromTree,
+} from "react-sortable-tree";
+import "react-sortable-tree/style.css";
+import { useState } from "react";
+import theme from "./theme";
+import uniqueString from "unique-string";
 
-function App() {
+const getNodeKey = ({ treeIndex }) => treeIndex;
+
+function App({ initialData, onSave, locale, height }) {
+  const [data, setData] = useState(() =>
+    getTreeFromFlatData({
+      flatData: initialData.map((node) => ({ ...node, title: node.name })),
+      getKey: (node) => node.id, // resolve a node's key
+      getParentKey: (node) => node.parent, // resolve a node's parent's key
+      rootKey: null, // The value of the parent key when there is no parent (i.e., at root level)
+    })
+  );
+
+  const createNewNode = () => ({ title: locale.empty, id: uniqueString() });
+
+  const handleChange = (newData) => {
+    setData(newData);
+  };
+
+  const handleAddRootItem = () => {
+    const newData = [createNewNode(), ...data];
+    setData(newData);
+  };
+
+  const handleAddChildItem = (path) => {
+    const { treeData: newData } = addNodeUnderParent({
+      treeData: data,
+      parentKey: path[path.length - 1],
+      expandParent: true,
+      getNodeKey,
+      newNode: createNewNode(),
+      addAsFirstChild: false,
+    });
+    setData(newData);
+  };
+
+  const handleRemoveItem = (path) => {
+    setData(
+      removeNodeAtPath({
+        treeData: data,
+        path,
+        getNodeKey,
+      })
+    );
+  };
+
+  const handleChangeTitle = (path, node, { target: { value } }) => {
+    setData(
+      changeNodeAtPath({
+        treeData: data,
+        path,
+        getNodeKey,
+        newNode: { ...node, title: value },
+      })
+    );
+  };
+
+  const handleExport = () => {
+    onSave(
+      getFlatDataFromTree({
+        treeData: data,
+        getNodeKey: ({ node }) => node.id,
+        ignoreCollapsed: false,
+      }).map(({ node, path }) => ({
+        id: node.id,
+        name: node.title,
+        // The last entry in the path is this node's key
+        // The second to last entry (accessed here) is the parent node's key
+        parent: path.length > 1 ? path[path.length - 2] : null,
+      }))
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div style={{ height }} className={"uk-padding-small uk-overflow-hidden"}>
+        <button
+          title={locale.add}
+          className={"uk-icon-button uk-margin-left"}
+          data-uk-icon="git-branch"
+          style={{ transform: "rotate(90deg)" }}
+          onClick={handleAddRootItem}
+        />
+        <SortableTree
+          treeData={data}
+          onChange={handleChange}
+          theme={theme}
+          generateNodeProps={({ node, path }) => ({
+            buttons: [
+              <button
+                className={"uk-icon-button"}
+                onClick={() => handleAddChildItem(path)}
+                data-uk-icon="git-branch"
+                style={{ transform: "rotate(90deg)" }}
+                key={"add"}
+              />,
+              <button
+                className={"uk-icon-button"}
+                onClick={() => handleRemoveItem(path)}
+                data-uk-icon="trash"
+                key={"remove"}
+              />,
+            ],
+            title: (
+              <input
+                className={"uk-input"}
+                value={node.title}
+                onChange={(event) => handleChangeTitle(path, node, event)}
+              />
+            ),
+          })}
+        />
+      </div>
+      <button
+        className={"uk-button uk-button-primary uk-margin-top"}
+        onClick={handleExport}
+      >
+        {locale.save}
+      </button>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+// import UIkit from "uikit";
+// import Icons from "uikit/dist/js/uikit-icons";
+// import "uikit/dist/css/uikit.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+export default function initUIKitSortableTree({
+  element = document.querySelector("[uk-tree]"),
+  data = [],
+  onSave = () => {},
+  locale = {
+    add: "Add",
+    save: "Save",
+    empty: "(Empty)",
+  },
+  height = 400,
+}) {
+  // UIkit.use(Icons);
+  if (element) {
+    ReactDOM.render(
+      <App
+        initialData={data}
+        onSave={onSave}
+        locale={locale}
+        height={height}
+      />,
+      element
+    );
+  } else {
+    console.error("Provided element is not valid");
+  }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// example
+// initUIKitSortableTree({
+//   element: document.getElementById("root"),
+//   data: [
+//     { id: 1, name: "Novinky", parent: null },
+//     { id: 2, name: "Galerie", parent: null },
+//     { id: 3, name: "Interiéry", parent: 2 },
+//     { id: 4, name: "Praha", parent: 3 },
+//   ],
+//   onSave: (data) => {
+//     console.log(data);
+//   },
+//   height: "80vh",
+//   locale: {
+//     add: "Přidat",
+//     save: "Uložit",
+//     empty: "(Prázdné)",
+//   },
+// });
