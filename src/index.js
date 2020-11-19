f90350286b54536bf77486df3050154f8f758908
Matous Trca <matous.trca@gmail.com>
2020-11-19 10:07:53 +0100

    hotfix

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import UIkit from "uikit";
-import Icons from "uikit/dist/js/uikit-icons";
-import "uikit/dist/css/uikit.css";
+// import UIkit from "uikit";
+// import Icons from "uikit/dist/js/uikit-icons";
+// import "uikit/dist/css/uikit.css";
 
 export default function initUIKitSortableTree({
   element = document.querySelector("[uk-tree]"),
@@ -19,7 +19,7 @@ export default function initUIKitSortableTree({
   },
   height = 400,
 }) {
-  UIkit.use(Icons);
+  // UIkit.use(Icons);
 
   if (!element) throw new Error("Provided element is not valid");
 
@@ -39,29 +39,29 @@ export default function initUIKitSortableTree({
   );
 }
 
-initUIKitSortableTree({
-  element: document.getElementById("root"),
-  data: [
-    { id: 1, title: { cs: "Novinky", en: "News" }, parent: null },
-    { id: 2, title: { cs: "Galerie", en: "Gallery" }, parent: null },
-    { id: 3, title: { cs: "Interiéry", en: "Interiors" }, parent: 2 },
-    { id: 4, title: { cs: "Praha", en: "Prague" }, parent: 3 },
-  ],
-  onSave: (data) => {
-    console.log(data);
-  },
-  onEdit: (id) => {
-    console.log(id);
-  },
-  height: "80vh",
-  locales: [
-    ["cs", "Čeština"],
-    ["en", "Angličtina"],
-  ],
-  defaultLocale: "cs",
-  translations: {
-    add: "Přidat",
-    save: "Uložit",
-    empty: "(Prázdné)",
-  },
-});
+// initUIKitSortableTree({
+//   element: document.getElementById("root"),
+//   data: [
+//     { id: 1, title: { cs: "Novinky", en: "News" }, parent: null },
+//     { id: 2, title: { cs: "Galerie", en: "Gallery" }, parent: null },
+//     { id: 3, title: { cs: "Interiéry", en: "Interiors" }, parent: 2 },
+//     { id: 4, title: { cs: "Praha", en: "Prague" }, parent: 3 },
+//   ],
+//   onSave: (data) => {
+//     console.log(data);
+//   },
+//   onEdit: (id) => {
+//     console.log(id);
+//   },
+//   height: "80vh",
+//   locales: [
+//     ["cs", "Čeština"],
+//     ["en", "Angličtina"],
+//   ],
+//   defaultLocale: "cs",
+//   translations: {
+//     add: "Přidat",
+//     save: "Uložit",
+//     empty: "(Prázdné)",
+//   },
+// });
